737db8618f591d40cfdcef55f620543f2ed059ae
zou zhi xin <1260526412@qq.com>
2022-07-20 18:47:53 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -53,7 +53,7 @@ Vue.use(Router);
 const routes = [
   {
     path: "/",
-    component: UyReportSearch,
+    component: FinancialForecasts,
     meta: {
       title: "test",
       requireAuth: false,
